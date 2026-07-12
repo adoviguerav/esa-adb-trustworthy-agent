d5088559aee972c0a708e5f9a3be1de6a6f44600
adolfoviguera <160146493+adoviguerav@users.noreply.github.com>
2026-07-12 19:18:35 +0200

    Delete fixtures.py

--- tests/fixtures.py
+++
@@ -1,8 +0,0 @@
-"""D7-conformant mock builders so [2]/[3]/[4] are testable before [1] is wired in.
-
-Skeleton only. Add mock_detection_result() / mock_detection() here once interfaces.py
-defines the D7 types. Structurally-real shapes, synthetic values, tests only.
-"""
-from __future__ import annotations
-
-# TODO: mock_detection_result(...), mock_detection(...) once D7 types exist.
